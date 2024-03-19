b629eec2a6bf01ed07046d72d016fde52890d183
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-19 10:29:13 +0300

    Added useNavigate

--- routing/src/components/Menu.js
+++ routing/src/components/Menu.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Menu() {
+    const navigate = useNavigate();
+
   return (
     <div>
         <ul className='menu'>
+            <li onClick={() => navigate(-1)}>Geri</li>
+            {/* <li>
+                <a href='#/' onClick={() => navigate(-1)}>Geri</a>
+            </li> */}
             <li>
                 <Link to="/">Ana Sayfa</Link>
             </li>
