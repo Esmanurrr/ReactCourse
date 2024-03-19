c0d9ada2872d957f8e190ed0e26a41f7157bad69
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-19 12:51:39 +0300

    Added layouts

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,21 +1,29 @@
-import Home from './pages/Home';
+import Home from './pages/Dashboard/Home';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Users from './pages/Users';
-import Contact from './pages/Contact';
-import Menu from './components/Menu';
-import UserDetail from './pages/UserDetail';
+import Users from './pages/Dashboard/Users';
+import Contact from './pages/Dashboard/Contact';
+import UserDetail from './pages/Dashboard/UserDetail';
+import Login from './pages/Auth/Login';
+import DashboardLayout from './layouts/Dashboard';
+import AuthLayout from './layouts/Auth';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Menu/>
         <Routes>
-          <Route index element={<Home/>} />
-          <Route path='users' element={<Users/>} />
-          <Route path='users/:id' element={<UserDetail/>} />
-          <Route path='contact' element={<Contact/>} />
+          <Route path="/" element={<DashboardLayout/>}>
+            <Route index element={<Home/>} />
+            <Route path='users' element={<Users/>} />
+            <Route path='users/:id' element={<UserDetail/>} />
+            <Route path='contact' element={<Contact/>} />
+          </Route>
+          <Route path='auth' element={<AuthLayout/>}>
+            <Route index element={<Login/>} />
+            <Route path='register' element={<Register/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
