9cf1b95ff7955bb06de45acc6d21df21d2c8b422
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-21 10:16:42 +0300

    Added useFormik hook

--- routing/src/pages/Dashboard/Contact.js
+++ routing/src/pages/Dashboard/Contact.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import {Formik, Field, Form} from 'formik'
+import {useFormik} from 'formik'
 
 function Contact() {
+  const {handleSubmit, handleChange} = useFormik({
+    initialValues:{
+      firstName:"",
+      lastName:"",
+      email:"",
+      message:""
+    },
+    onSubmit:(values) => {
+      console.log(values);
+    }}
+  )
+
   return (
     <div>
         <h2>İletişim</h2>
 
-        <Formik
-          initialValues={{
-            firstName:"",
-            lastName:"",
-            email:"",
-          }}
-          onSubmit={(values) => {
-            console.log(JSON.stringify(values));
-          }}
-        >
-          <Form>
-            <label htmlFor='firstName'>First Name: </label>
-            <Field id="firstName" name="firstName" placeholder="Jane" />
+          <form onSubmit={handleSubmit} className='form'>
+            <div>
+              <label htmlFor='firstName'>First Name: </label>
+              <input id="firstName" name="firstName" placeholder="Jane" onChange={handleChange("firstName")} />
+            </div>
 
-            <label htmlFor='lastName'>Last Name: </label>
-            <Field id="lastName" name="lastName" placeholder="Doe" />
+            <div>
+              <label htmlFor='lastName'>Last Name: </label>
+              <input id="lastName" name="lastName" placeholder="Doe" onChange={handleChange("lastName")}/>
+            </div>
 
-            <label htmlFor='email'>Email: </label>
-            <Field id="email" name="email" placeholder="jane@acme.com" />
+            <div>
+              <label htmlFor='email'>Email: </label>
+              <input id="email" name="email" placeholder="jane@acme.com" onChange={handleChange("email")}/>
+            </div>
+
+            <div>
+              <label htmlFor='message'>Message: </label>
+              <textarea id="message" name="message" placeholder="Text message" onChange={handleChange("message")}/>
+            </div>
+
+
             <button type='submit'>Submit</button>
-          </Form>
-        </Formik>
+          </form>
     </div>
   )
 }
